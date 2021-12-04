7210f46e50f89d1e818f637a2701e9bb2c60ce76
Ernest Leitch <metric@152.org>
2021-12-04 10:52:56 -0800

    use transitions for animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, transition, style, animate, query } from '@angular/animations';
+import { trigger, transition, style, animate, query, AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -7,30 +7,52 @@ import { trigger, transition, style, animate, query } from '@angular/animations'
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('newBlockElement', [
-      transition(':enter', [
-        style({ opacity: 0, borderColor: 'red', transform: 'translateX(100%)'}),
-        animate('500ms', style({opacity: 1, borderColor: 'black', transform: 'translateX(0)'}))
+      transition('* => forward', [
+        query(':enter', [
+          style({ opacity: 0, borderColor: 'red', transform: 'translateX(100%)' }),
+          animate('500ms', style({ opacity: 1, borderColor: 'black', transform: 'translateX(0)' }))
+        ], { optional: true }),
+        query(':leave', [
+          animate('500ms', style({ opacity: 0, borderColor: 'red', transform: 'translateX(-100%)', position: 'absolute' }))
+        ], { optional: true })
       ]),
-      transition(':leave', [
-        animate('5000ms', style({opacity: 0, borderColor: 'red', transform: 'translateX(-100%)', position: 'releative'}))
-      ])
-    ])]
+    ])
+  ]
 })
 export class AppComponent {
+  /**
+   * We need to query the :enter/:leaving element
+   * https://angular.io/guide/complex-animation-sequences#filter-animation-example
+   * https://javascript.plainenglish.io/more-complex-angular-animations-47015a42a8d1
+   */
   title = 'ng-animation';
 
-  blocks = [1,2,3];
+  blocks = [1, 2, 3];
+  direction: string = '';
+
+  DIRECTION_FORWARD = 1;
+  DIRECTION_BACKWARD = -1;
 
   update(direction: number) {
+    // This is backwards
     if (direction < 0 && this.blocks[0] > 0) {
       this.blocks.pop();
       this.blocks.unshift(this.blocks[0] - 1);
+      this.direction = 'backwards';
     }
 
-    if (direction > 0){
+    // This is forwards
+    if (direction > 0) {
       this.blocks.shift();
       this.blocks.push(this.blocks[this.blocks.length - 1] + 1);
+      this.direction = 'forward';
     }
+  }
+
+  onAnimation($event: AnimationEvent) {
+    console.log(`event:`, $event)
+    // Clear the direction when the animation finishes
+    this.direction = '';
   }
 
 }
